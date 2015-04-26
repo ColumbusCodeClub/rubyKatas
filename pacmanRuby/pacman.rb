ae7849dd1a4e009f8a4b9d7484c001829b9b69c1
scoobycoder <scoobycoder@gmail.com>
2015-04-26 16:25:36 -0400

    simple cleanup refactorings

--- pacmanRuby/pacman.rb
+++ pacmanRuby/pacman.rb
@@ -10,44 +10,8 @@ class Pacman
 
   END_OF_BOARD = 10
 
-  MOVES = {
-      :east => lambda { |this| this.tick_east },
-      :north => lambda { |this| this.tick_north },
-      :west => lambda { |this| this.tick_west },
-      :south => lambda { |this| this.tick_south }
-  }
-
   def tick
     MOVES[@direction].call(self)
-  end
-
-  def move(east_west, north_south)
-    original_location = []
-    original_location[0] = @location[0]
-    original_location[1] = @location[1]
-
-    @location[0] += east_west
-    @location[1] += north_south
-    if (@location[0] < 0)
-      @location[0] = @world.size-1
-    end
-    if (@location[1] < 0)
-      @location[1] = @world.size-1
-    end
-    if (@location[1] == @world.size)
-      @location[1] = 0
-    end
-    if (@location[0] == @world.size)
-      @location[0] = 0
-    end
-
-    if @world[@location[0]][@location[1]] != :wall
-      @world[original_location[0]][original_location[1]] = nil
-      @world[@location[0]][@location[1]] = self
-    else
-      @location[0] = original_location[0]
-      @location[1] = original_location[1]
-    end
   end
 
   def tick_south
@@ -73,4 +37,68 @@ class Pacman
       @direction = direction
     end
   end
+
+  private
+
+  MOVES = {
+      :east => lambda { |this| this.tick_east },
+      :north => lambda { |this| this.tick_north },
+      :west => lambda { |this| this.tick_west },
+      :south => lambda { |this| this.tick_south }
+  }
+
+  def move(east_west, north_south)
+    original_location = []
+    original_location[0] = @location[0]
+    original_location[1] = @location[1]
+
+    @location[0] += east_west
+    @location[1] += north_south
+    if (@location[0] < 0)
+      @location[0] = @world.size-1
+    end
+    if (@location[1] < 0)
+      @location[1] = @world.size-1
+    end
+    if (@location[1] == @world.size)
+      @location[1] = 0
+    end
+    if (@location[0] == @world.size)
+      @location[0] = 0
+    end
+
+    check_for_walls(original_location)
+  end
+
+  def check_for_walls(original_location)
+    if no_wall_exists
+      continue_move(original_location)
+    else
+      return_to_original_location(original_location)
+    end
+  end
+
+  def return_to_original_location(original_location)
+    @location[0] = original_location[0]
+    @location[1] = original_location[1]
+  end
+
+  def continue_move(original_location)
+    clear_pacman_location(original_location)
+    enter_new_pacman_location
+  end
+
+  def enter_new_pacman_location
+    @world[@location[0]][@location[1]] = self
+  end
+
+  def clear_pacman_location(original_location)
+    @world[original_location[0]][original_location[1]] = nil
+  end
+
+  def no_wall_exists
+    @world[@location[0]][@location[1]] != :wall
+  end
+
+
 end
